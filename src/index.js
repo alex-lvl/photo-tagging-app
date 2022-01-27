c786b17a25d95e647503e6ff8a58dbcacac823b8
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-01-27 02:42:15 -0500

    initialize app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import Nav from './components/nav';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBN-JllOYRcr_rO-VF1MWU7XOjZkTXyuAM",
+  authDomain: "photo-tagging-3a70f.firebaseapp.com",
+  projectId: "photo-tagging-3a70f",
+  storageBucket: "photo-tagging-3a70f.appspot.com",
+  messagingSenderId: "914940221076",
+  appId: "1:914940221076:web:6f25f3c57f22a785d98681"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
+    <Nav />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
