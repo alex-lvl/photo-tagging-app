2772f86ce0925d72830ed7d6664a0bb412013a31
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-01-29 03:48:32 -0500

    added route switch logic to games

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import { useState } from 'react/cjs/react.development';
+import { useNavigate } from "react-router-dom";
 import './App.css';
+import Game from './components/Game';
+import beautyGame from './images/princess-beauty.png';
+import cinderellaGame from './images/princess-cinderella.png';
 
 function App() {
+  let navigate = useNavigate();
+  
+  const handleClick = async (e) => {
+      let game = e.target
+      console.log(game);
+      navigate(`/game/${game.id}`);
+
+    // switch (e.target.src) {
+    //   case beautyGame:
+    //     console.log('beauty');
+    //     break;
+    //   case cinderellaGame:
+    //     console.log('cinderella');
+    //     break;
+    //   default:
+    //     console.log('no matchin cases');
+    // }
+  };
+
   return (
     <div className="App">
-
+      <div className="games">
+        <Game
+          image={beautyGame}
+          handleClick={handleClick}
+          alt={'sleeping beauty game option'}
+          id={1}
+        />
+        <Game
+          image={cinderellaGame}
+          handleClick={handleClick}
+          alt={'cinderella game option'}
+          id={2}
+        />
+      </div>
     </div>
   );
 }
