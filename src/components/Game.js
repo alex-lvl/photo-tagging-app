0a8482a9098da440e67707bc12b966f77bd51b60
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-01-30 03:13:03 -0500

    fixed bug where stopwatch continued tick at different routes

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
+
 function Game(props) {
+  useEffect(() => {
+    props.setGameOver(false);
+  }, [props.gameOver]);
+  useEffect(props.handleStart, [props.gameOver]);
+
   const characters = props.characters.map((character) => (
     <div className="character" key={character.id}>
-      <img src={character.src} alt={character.name}/>
+      <img src={character.src} alt={character.name} />
       <span>{character.name}</span>
     </div>
   ));
@@ -9,7 +16,7 @@ function Game(props) {
   return (
     <div>
       <div className="game">
-        <img src={props.image} alt={props.alt} id={props.id}/>
+        <img src={props.image} alt={props.alt} id={props.id} />
       </div>
       <div className="characters-container">{characters}</div>
     </div>
