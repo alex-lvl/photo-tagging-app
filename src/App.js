042dee1b8e1d8123bd3040c3ac7f8f841d600d95
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-01-29 20:09:24 -0500

    changed component name Game to Options

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useState } from 'react/cjs/react.development';
 import { useNavigate } from "react-router-dom";
 import './App.css';
-import Game from './components/Game';
+import Options from './components/Options';
 import beautyGame from './images/princess-beauty.png';
 import cinderellaGame from './images/princess-cinderella.png';
 
 function App() {
   let navigate = useNavigate();
-  
+
   const handleClick = async (e) => {
       let game = e.target
       console.log(game);
@@ -28,13 +28,13 @@ function App() {
   return (
     <div className="App">
       <div className="games">
-        <Game
+        <Options
           image={beautyGame}
           handleClick={handleClick}
           alt={'sleeping beauty game option'}
           id={1}
         />
-        <Game
+        <Options
           image={cinderellaGame}
           handleClick={handleClick}
           alt={'cinderella game option'}
