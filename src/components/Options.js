a41343ab5d84189623c04f0e7aae6616c5b4c372
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-02-05 03:17:41 -0500

    added more information to game options

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,7 +1,22 @@
 function Options(props) {
+  const characters = props.characters.map((character) => (
+    <div className="character" id={character.id} key={character.id}>
+      <img
+        src={character.src}
+        alt={character.name}
+        className="character-image"
+      />
+      <span className="character-name">{character.name}</span>
+    </div>
+  ));
+
   return (
-    <div className="game-container" onClick={props.handleClick}>
+    <div className="game-container">
       <img src={props.image} alt={props.alt} id={props.id} />
+      <h4>{props.game}</h4>
+      <p>Find these characters:</p>
+      {characters}
+      <button className="start-btn" id={props.id} onClick={props.handleClick}>start game</button>
     </div>
   );
 }
