eafaf4824da2e9292f4c79d34cf79923da4d31c5
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-02-05 03:15:34 -0500

    added more information to game options and set handle start

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ function App(props) {
   let navigate = useNavigate();
   const handleClick = async (e) => {
     let game = e.target;
-    console.log(game);
-    props.setGameId(game.id)
+    props.setGameId(game.id);
+    props.handleStart();
     navigate(`/game/${game.id}`);
   };
 
   return (
     <div className="App">
+      <h2>Choose a game to play</h2>
       <div className="games">
         <Options
+          characters={props.games[0].characters}
+          game={'sleeping beauty'}
           image={props.beautyGame}
           handleClick={handleClick}
           alt={'sleeping beauty game option'}
           id={0}
         />
         <Options
+          characters={props.games[1].characters}
+          game={'cinderella'}
           image={props.cinderellaGame}
           handleClick={handleClick}
           alt={'cinderella game option'}
