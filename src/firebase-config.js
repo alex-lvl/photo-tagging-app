1d27ee53a5aa671d8b297d94ad938762a1632c20
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-02-09 19:56:24 -0500

    initialized firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBN-JllOYRcr_rO-VF1MWU7XOjZkTXyuAM",
+  authDomain: "photo-tagging-3a70f.firebaseapp.com",
+  projectId: "photo-tagging-3a70f",
+  storageBucket: "photo-tagging-3a70f.appspot.com",
+  messagingSenderId: "914940221076",
+  appId: "1:914940221076:web:6f25f3c57f22a785d98681"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
