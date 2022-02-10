2d94a7117a2594fbeaf91ab04e9afbcef5559362
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-02-10 02:06:48 -0500

    added read data functionality and displayed to DOM

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,9 +1,43 @@
+import {
+  query,
+  orderBy,
+  collection,
+  where,
+  limit,
+  getDocs,
+} from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase-config';
 import Levels from './Levels';
 
 function Leaderboard(props) {
-  const getLeaders = () => {
-    console.log('click');
+  const [leaders, setLeaders] = useState([]);
+  const [title, setTitle] = useState('Click a game to display scores');
+  useEffect(() => {
+    const leaderboardTitle = document.querySelector('.leaderboard-title');
+    leaderboardTitle.textContent = title;
+  }, [title]);
+
+  async function getLeaders(id, title) {
+    let scoreQuery = query(
+      collection(db, `scores${id}`),
+      where('score', '>', 0),
+      orderBy('score'),
+      limit(100)
+    );
+    const querySnapshot = await getDocs(scoreQuery);
+    const leaderboard = querySnapshot.docs.map((doc) => doc.data());
+    console.log(leaderboard, 'firestore scores');
+    setTitle(title);
+    return setLeaders(leaderboard);
   }
+
+  const displayLeaders = leaders.map((leader, i) => (
+    <tr key={i}>
+      <td>{leader.name}</td>
+      <td>{leader.score}</td>
+    </tr>
+  ));
 
   return (
     <div className="leaderboard">
@@ -14,7 +48,7 @@ function Leaderboard(props) {
           image={props.beautyGame}
           alt={'sleeping beauty game option'}
           getLeaders={getLeaders}
-          id={0}
+          id={1}
         />
         <Levels
           characters={props.games[1].characters}
@@ -22,10 +56,11 @@ function Leaderboard(props) {
           image={props.cinderellaGame}
           alt={'cinderella game option'}
           getLeaders={getLeaders}
-          id={1}
+          id={2}
         />
       </div>
 
+      <h2 className="leaderboard-title">Click a game to display scores</h2>
       <table>
         <colgroup>
           <col span="1" style={{ width: '60%' }} />
@@ -38,12 +73,7 @@ function Leaderboard(props) {
             <th colSpan="1">time (seconds)</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>Alex</td>
-            <td>101.01</td>
-          </tr>
-        </tbody>
+        <tbody>{displayLeaders}</tbody>
       </table>
     </div>
   );
