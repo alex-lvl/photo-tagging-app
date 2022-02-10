26c6bc92e9045ad93b55b61916620b8217b6220f
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-02-10 02:13:04 -0500

    removed unnecessary import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import RouteSwitch from './RouteSwitch';
 import reportWebVitals from './reportWebVitals';
-import { app } from './firebase-config';
 
 ReactDOM.render(
   <React.StrictMode>
