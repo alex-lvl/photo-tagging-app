eb344dcbd9992233794b5e4b7b01fcbf43aad115
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-02-09 19:56:04 -0500

    imported firebase from firebase config

--- src/index.js
+++ src/index.js
@@ -3,22 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import RouteSwitch from './RouteSwitch';
 import reportWebVitals from './reportWebVitals';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBN-JllOYRcr_rO-VF1MWU7XOjZkTXyuAM",
-  authDomain: "photo-tagging-3a70f.firebaseapp.com",
-  projectId: "photo-tagging-3a70f",
-  storageBucket: "photo-tagging-3a70f.appspot.com",
-  messagingSenderId: "914940221076",
-  appId: "1:914940221076:web:6f25f3c57f22a785d98681"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+import { app } from './firebase-config';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -26,14 +11,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Nav />
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
