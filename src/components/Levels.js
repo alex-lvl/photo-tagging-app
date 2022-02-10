a62f8009a2156ba084098230e45751786c93d4cc
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-02-10 02:07:39 -0500

    cleaned code

--- src/components/Levels.js
+++ src/components/Levels.js
@@ -1,7 +1,14 @@
 function Levels(props) {
   return (
     <div className="game-container">
-      <img src={props.image} alt={props.alt} id={props.id} onClick={props.getLeaders} />
+      <img
+        src={props.image}
+        alt={props.alt}
+        id={props.id}
+        onClick={() => {
+          props.getLeaders(props.id, props.game);
+        }}
+      />
       <h4>{props.game}</h4>
     </div>
   );
